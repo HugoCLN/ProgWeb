24a8374b2a82cc47ec1e5fb7d2fff967772b48d1
Gatien Jost <82646847+GatienJ@users.noreply.github.com>
2022-09-30 16:47:50 +0200

    ajout service

--- src/app/user-api.service.ts
+++ src/app/user-api.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUser() {
+    return this.http
+      .get('https://randomuser.me/api/?results=20')
+  }
+
+  getUsers(amount: number) {
+    return this.http
+      .get(`https://randomuser.me/api/?results=${amount}`)
+      .pipe(map((res:any) => res.results))
+  }
 }
