3412ac047b39cce77493e5f1ebbdab598ce684b5
Gatien Jost <82646847+GatienJ@users.noreply.github.com>
2022-10-04 11:52:25 +0200

    Ajout reload

--- src/app/swipe/swipe.component.ts
+++ src/app/swipe/swipe.component.ts
@@ -5,44 +5,46 @@ import { UserApiService } from '../user-api.service';
 @Component({
   selector: 'app-swipe',
   templateUrl: './swipe.component.html',
-  styleUrls: ['./swipe.component.scss']
+  styleUrls: ['./swipe.component.scss'],
 })
 export class SwipeComponent implements OnInit {
+  nbusers: number = 5;
+  user_index: number = 0;
+  credits: number = 5;
 
-  nbusers : number = 5
-  user_index : number = 0
+  users: Array<User>;
+  favusers: Array<User>;
 
-  users: Array<User>
-  favusers : Array<User>
-
-  constructor(private userApi: UserApiService) { }
+  constructor(private userApi: UserApiService) {}
 
   ngOnInit(): void {
-    this.getUsers(this.nbusers)
+    this.getUsers(this.nbusers);
   }
 
-  getUsers(amount: number){
-    this.userApi.getUsers(amount).subscribe((results: Array<User>) =>{
-      this.users = results
+  getUsers(amount: number) {
+    this.userApi.getUsers(amount).subscribe((results: Array<User>) => {
+      this.users = results;
     });
   }
 
-  next_person(button_pushed:boolean, user: User)
-  {
-    if(this.user_index < this.nbusers){
-      this.getUsers(this.nbusers);
-      this.user_index = 0;
-    }
-    else{
+  next_person(button_pushed: boolean, user: User) {
+    if (this.user_index > this.nbusers) {
       this.user_index = this.user_index + 1;
-    }
-    if (button_pushed){
-      this.add_favuser(user);
+      if (button_pushed) {
+        this.add_favuser(user);
+      }
     }
   }
 
-  add_favuser(user : User)
-  {
-    this.favusers.push(user)
+  add_favuser(user: User) {
+    this.favusers.push(user);
+  }
+
+  reload_users() {
+    if (this.credits > 0) {
+      this.getUsers(this.nbusers);
+      this.user_index = 0;
+      this.credits--;
+    }
   }
 }
